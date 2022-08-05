483d4381624c0c1c0a9f3c9ccbdcdc04153ba370
Jaron <jjkenyon777@gmail.com>
2022-08-05 11:55:45 -0700

    working with props with typescript

--- react-ts/src/components/Todos.tsx
+++ react-ts/src/components/Todos.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
 
-function Todos() {
+//React.FC is a predefined generic type that stands for react functional component
+//inside the <> we define what kind of props are expected to be passed in 
+//in this case an object with a key/val pair of items which is an array of strings 'string[]'
+const Todos: React.FC<{items: string[]}> = (props) => {
   return (
     <ul>
-        <li>Learn React</li>
-        <li>Learn TypeScript</li>
+        {props.items.map(item => 
+            (<li key={item}>{item}</li>)
+            )}
     </ul>
   )
 }
