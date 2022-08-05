314c3f0ab505c76c5a5531914b1edd084b7f40f5
Jaron <jjkenyon777@gmail.com>
2022-08-05 12:07:24 -0700

    creating data model

--- react-ts/src/components/Todos.tsx
+++ react-ts/src/components/Todos.tsx
@@ -1,12 +1,14 @@
 
+import Todo from "../models/todo";
+
 //React.FC is a predefined generic type that stands for react functional component
 //inside the <> we define what kind of props are expected to be passed in 
-//in this case an object with a key/val pair of items which is an array of strings 'string[]'
-const Todos: React.FC<{items: string[]}> = (props) => {
+//in this case an object with a key/val pair of items which is a custom class object defined in the models folder
+const Todos: React.FC<{items: Todo[]}> = (props) => {
   return (
     <ul>
         {props.items.map(item => 
-            (<li key={item}>{item}</li>)
+            (<li key={item.id}>{item.text}</li>)
             )}
     </ul>
   )
